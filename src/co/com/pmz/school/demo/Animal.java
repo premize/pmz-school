b7b0e9bedf72060c64767b1a06ae2f07d286b251
Elmer Jose Diaz Lazo <elmerdiazlazo@gmail.com>
2013-07-05 15:34:56 -0500

    se ha agregado la clase animal

---
+++ src/co/com/pmz/school/demo/Animal.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package co.com.pmz.school.demo;
+
+/**
+ * @author <a href="mailto:elmerdiazlazo@gmail.com">Elmer Jose Diaz Lazo</a>
+ * @project pmz-school
+ * @class Animal
+ * @date 5/07/2013
+ *
+ */
+public class Animal {
+
+	private String tipo;
+	
+	
+	private String numeroPatas;
+
+
+	/**
+	 * @author <a href="mailto:elmerdiazlazo@gmail.com">Elmer Jose Diaz Lazo</a>
+	 * @date 2/06/2013
+	 * @return the tipo
+	 */
+	public String getTipo() {
+		return tipo;
+	}
+
+
+	/**
+	 *@author <a href="mailto:elmerdiazlazo@gmail.com">Elmer Jose Diaz Lazo</a>
+	 *@date 2/06/2013
+	 * @param tipo the tipo to set
+	 */
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+
+	/**
+	 * @author <a href="mailto:elmerdiazlazo@gmail.com">Elmer Jose Diaz Lazo</a>
+	 * @date 2/06/2013
+	 * @return the numeroPatas
+	 */
+	public String getNumeroPatas() {
+		return numeroPatas;
+	}
+
+
+	/**
+	 *@author <a href="mailto:elmerdiazlazo@gmail.com">Elmer Jose Diaz Lazo</a>
+	 *@date 2/06/2013
+	 * @param numeroPatas the numeroPatas to set
+	 */
+	public void setNumeroPatas(String numeroPatas) {
+		this.numeroPatas = numeroPatas;
+	}
+	
+	
+	
+	
+	
+}
+
+
